8f6eaecab3f4b88f1109faf043338d83626671df
vv-sam <vsamoilenko17@gmail.com>
2025-01-29 19:37:56 +0300

    chess board task

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("new feature released")
+	fmt.Print("Chess board size (i.e. \"8x8\"): ")
+	l, w := 0, 0
+	_, err := fmt.Scanf("%dx%d", &l, &w)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c := 0
+
+	sb := strings.Builder{}
+	for i := 0; i < l; i++ {
+		for j := 0; j < w; j++ {
+			if c%2 == 0 {
+				sb.WriteByte('#')
+			} else {
+				sb.WriteByte(' ')
+			}
+			c++
+		}
+		if w%2 == 0 {
+			c++
+		}
+		sb.WriteByte('\n')
+	}
+	fmt.Println(sb.String())
 }
